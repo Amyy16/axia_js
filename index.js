92005a0a161e8b34a2e59ec2e4c2d7a74bed7c6f
Amarachi <nwabueze2amarachi@gmail.com>
2024-07-29 20:11:23 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const myStudent = ["ben", "onyeka", "chizoba", "anne", "ade", "benita", "ann", "john","praise", "ogechi"];
+
+const user = {
+    name: "amara",
+    age: 15,
+    married: true,
+    location: undefined
+}
+
+for (let i = 0; i < myStudent.length; i++) {
+    console.log(myStudent[i]);
+    
+}
+
+console.log(user.name);
+console.log(user.age);
+console.log(user.married);
+console.log(user.location);
